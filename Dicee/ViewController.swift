1143a920b3cc7f1fe199288e634f784a4e2a8869
mllsumulong <mllsumulong@gmail.com>
2020-04-07 23:31:50 -0700

    Name    :   Maricel Louise Sumulong Date      :   April 07, 2020 PDT E-mail   :   mllsumulong@gmail.com
    Changes:
    - removed notes during lecture

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -13,10 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     
     @IBOutlet weak var diceImageView2: UIImageView!
-    //when you rename the variable, break it by right-clicking the image, click the 'X' then reconnect it by dragging the circle with the image
     
-    var leftDiceNumber = 1
-    var rightDiceNumber = 4
     var diceArray = [#imageLiteral(resourceName: "dice1"),#imageLiteral(resourceName: "dice2"),#imageLiteral(resourceName: "dice3"),#imageLiteral(resourceName: "dice4"),#imageLiteral(resourceName: "dice5"),#imageLiteral(resourceName: "dice6")]
     
     //var keyword is for something that changes in the code otherwise use let or const
@@ -26,8 +23,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
    
-        diceImageView2.image = #imageLiteral(resourceName: "dice6"); //type image literal to show the icon
-        //diceImageView2.alpha = 0.5;
+        diceImageView2.image = #imageLiteral(resourceName: "dice6");
         
     }
 
@@ -36,16 +32,6 @@ class ViewController: UIViewController {
         diceImageView1.image = diceArray.randomElement()
     
         diceImageView2.image = diceArray.randomElement()
-        //array.shuffle() can also be used
-        
-        // can also use Int.random(in: a...z) where a is the start and z is the end of the number
-        
-        //diceImageView1.image = [#imageLiteral(resourceName: "dice1"),#imageLiteral(resourceName: "dice2"),#imageLiteral(resourceName: "dice3"),#imageLiteral(resourceName: "dice4"),#imageLiteral(resourceName: "dice5"),#imageLiteral(resourceName: "dice6")][leftDiceNumber]
-        
-        //diceImageView2.image = [#imageLiteral(resourceName: "dice1"),#imageLiteral(resourceName: "dice2"),#imageLiteral(resourceName: "dice3"),#imageLiteral(resourceName: "dice4"),#imageLiteral(resourceName: "dice5"),#imageLiteral(resourceName: "dice6")][rightDiceNumber]
-        
-        //leftDiceNumber = leftDiceNumber + 1
-        //rightDiceNumber = rightDiceNumber - 1
         
     }
     
